0ae7465c72f3d215e941e31ac87d6ada902af7dc
trymha <47079832+trymha@users.noreply.github.com>
2022-12-18 14:29:45 +0100

    inital commit

---
+++ histogram_plot.py
@@ -0,0 +1,109 @@
+import numpy as np
+import pandas as pd
+from IPython.display import display
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from matplotlib.dates import DateFormatter
+import datetime
+import re
+import plotly.graph_objects as go
+import plotly.express as px
+import math
+from matplotlib.ticker import FormatStrFormatter
+from datetime import datetime, timedelta
+pd.set_option("display.precision", 0)
+
+df = pd.read_csv('donations-effekt.csv', delimiter=";", parse_dates=['timestamp_confirmed'])
+df = df.sort_values(by='timestamp_confirmed')
+df.index = pd.to_datetime(df['timestamp_confirmed'])
+
+MDs = pd.DataFrame() #monthly donations
+MDs['sum_confirmed'] = df['sum_confirmed'].resample('M').sum() #Resample by day
+MDs['date_name'] = [ts.month_name()[:3] + ' ' + str(ts.year) for ts in MDs.index]
+MDs['timestamp'] = MDs.index
+MDs.index = [i for i in range(0,len(MDs))]
+#df['timestamp_confirmed'] = pd.to_datetime(df['timestamp_confirmed'])
+
+cumulative_donations = np.cumsum( df['sum_confirmed'].to_numpy() )
+donations = df['sum_confirmed']
+timestamps = pd.to_datetime(df['timestamp_confirmed'])
+
+hvr_tmpl = '<b>Donasjonsmenge:</b> %{x} NOK<br><b>Prosent:</b> %{y:.2f}%<extra></extra>'
+
+def get_df_subset_by_month(month_index_range):
+    all_dates = MDs.iloc[month_index_range[0]:month_index_range[1]]
+    date_1 = all_dates['timestamp'].iloc[0]
+    date_2 = all_dates['timestamp'].iloc[-1]
+    df_subset = df.loc[(df.index>date_1)&(df.index<date_2)]
+    return df_subset
+
+def get_histogram(month_index_range, n_bins=100):
+    df_subset = get_df_subset_by_month(month_index_range)
+    donations = df_subset['sum_confirmed'].to_numpy()
+    fig = go.Figure()
+    fig.add_trace(
+        go.Histogram(x=donations, nbinsx=n_bins, marker_color='black', histnorm='percent', hovertemplate=hvr_tmpl)
+    )
+    fig.update_layout(
+        xaxis = dict(title=dict(text='Donasjonsmenge [NOK]'), fixedrange=True),
+        yaxis = dict(type='log', title=dict(text='Prosent av donasjoner [%]'), fixedrange=True)
+    )
+    return fig
+
+def get_barplot(month_index_range):
+    flip_single = True #Flip single donations
+    df_subset = get_df_subset_by_month(month_index_range)
+    df_recurring, df_single = pd.DataFrame(), pd.DataFrame()
+    df_recurring['recurring'] = MDs['sum_confirmed'].apply(lambda x: x/1000)
+    df_recurring.index = MDs['timestamp']
+    if flip_single:
+        df_single['single'] = MDs.loc[MDs.index.get_level_values(0)==False]['sum_confirmed'].apply(lambda x: -x/1000)
+    else:
+        df_single['single'] = MDs.loc[MDs.index.get_level_values(0)==False]['sum_confirmed'].apply(lambda x: -x/1000)
+    #df_recurring.index = df_recurring.index.droplevel(0)
+    #df_single.index = df_single.index.droplevel(0)
+
+    step = 400
+    #y_max = int(math.ceil((max(df_single['single'].max(),df_recurring['recurring'].max())*1.05)/step)*step) #round max value to nearest hundred
+    bar_w= 26 if flip_single else 14
+    bar_w_dt = timedelta(days=bar_w)
+    fig = go.Figure()
+    if flip_single:
+        fig.add_trace(
+            go.Bar(x=df_recurring.index-bar_w_dt/2, y=df_recurring['recurring'], marker_color='black')#, width=bar_w)#, color='k',label='Recurring')
+        )
+        # fig.add_trace(
+        #     go.Bar(x=df_single.index-bar_w_dt/2, y=df_single['single'], marker_color='grey')
+        # )
+        #ax.bar(x=df_single.index-bar_w_dt/2, height=df_single['single'], width=bar_w, color='grey',label='Single')
+    # else:
+    #     ax.bar(x=df_recurring.index-bar_w_dt/2,height=df_recurring['recurring'], width=bar_w, color='k',label='Recurring')#Shift left by half
+    #     ax.bar(x=df_single.index-bar_w_dt*3/2,height=df_single['single'], width=bar_w, color='grey',label='Single')#Shift left by 3/2
+    # ax.bar(x=df_recurring.index,height=df_recurring['recurring'], width=bar_w, color='k',label='Recurring')
+    # ax.bar(x=df_single.index,height=df_single['single'], width=bar_w, color='grey',label='Single')
+    # ax.set_ylim(bottom=-y_max, top=y_max)
+    # ax.set_yticks([x for x in range(-y_max,y_max+step,step)])
+    # ax.xaxis.set_major_formatter( mdates.DateFormatter('%b-%y') )
+    # ax.yaxis.set_major_formatter(FormatStrFormatter('% .0f'))
+    # [plt.axhline(y,color='white',linewidth=0.5) for y in ax.get_yticks()]
+    # #plt.axhline(0,color='grey',linewidth=0.5)
+    # plt.ylabel('Donations [kNok]')
+    # ax.yaxis.tick_right()
+    # ax.yaxis.set_label_position("right")
+    # ax.set_yticklabels([f'{abs(x)}' for x in ax.get_yticks()])
+    # #ax.yaxis.get_major_ticks()
+    # for key, spine in ax.spines.items():
+    #     spine.set_visible(False)
+    # plt.tight_layout(True)
+    #[fig.add_hline(y=y_tick) for y_tick in 
+    fig.update_layout(
+        dict(
+            plot_bgcolor='rgba(0, 0, 0, 0)',
+            yaxis=dict(
+                showgrid=True,
+                gridcolor='White'
+                )
+        )
+    )
+    #fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='LightPink')
+    return fig
---
+++ test_app.py
@@ -0,0 +1,85 @@
+from dash import Dash, html, dcc, Output, Input
+import dash_bootstrap_components as dbc
+#from organization_plots import *
+from histogram_plot import *
+
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+
+#month_marks = {i: ts.month_name()[:3] + ' ' + str(ts.year) for i,ts in enumerate(timestamps)}
+showlabel_months = ['January','April','July','October']
+month_marks = {i: {
+    'label':ts.month_name()[:3] + '\n' +  str(ts.year),
+    'style': {'white-space':'nowrap','writing-mode': 'vertical-rl'}} for i,ts in enumerate(MDs['timestamp'])}
+    #'style': {'white-space':'nowrap'}} for i,ts in enumerate(MDs['timestamp'])}
+#month_marks = {i: '''a<br>a''' for i,ts in enumerate(MDs['timestamp'])}
+time_options = ['Week','Year','Quarter','Month']
+time_dd = dcc.Dropdown(id='time-dd',value=time_options[0],options=[{'label':to,'value':to} for to in time_options])
+
+month_slider = dcc.RangeSlider(
+        id='month-slider', 
+        marks=month_marks, 
+        value=[0,len(month_marks)-1], step=None, min=0, max=len(month_marks),
+        allowCross= False, pushable=6, #Minimum 6 months range
+        #vertical=True, verticalHeight=700,
+        #tooltip={"placement": "bottom", "always_visible": True},
+        className='slider'),
+
+year_slider = dcc.RangeSlider(
+        id='year-slider', 
+        #marks=month_marks, 
+        value=[2019,2021], step=1, min=2018, max=2022,
+        marks=None,
+        #vertical=True, verticalHeight=700,
+        #tooltip={"placement": "bottom", "always_visible": True},
+        )#className='slider'),
+
+histogram_graph = dcc.Graph(id='histogram-graph', style={"width": "50%"})
+bar_graph = dcc.Graph(id='bar-graph', style={"width": "50%"})
+
+app.layout = dbc.Container([
+    #time_dd,
+    #dcc.Slider(id='bins-slider', value=100, min=20, max=500, step=10, className='slider'),
+    html.Br(),
+    dbc.Row([
+       # dbc.Col(histogram_graph, width={"size": 6},style = {'background-color':'cyan'}),
+        #dbc.Col(bar_graph, width={"size": 6},style = {'background-color':'cyan'}),
+        #dbc.Col([year_slider], style={'width':'30%'}),
+        dbc.Col(html.Span('2019', id='from-year'), width='auto'),
+        dbc.Col(year_slider, width=4),
+        dbc.Col(html.Span('2021', id='to-year'), width='auto'),
+        #dbc.Col([dcc.RangeSlider(0, 20, marks=None, value=[5, 15])], style={'width':'30%'})
+    ]), #style = {"height": "100%", 'background-color':'yellow'}), 
+    dbc.Row(dbc.Col(histogram_graph)),
+    # dbc.Row(year_slider), 
+    html.Br(),
+    ]
+)
+
+@app.callback(
+    Output('histogram-graph','figure'),
+    Input('year-slider','value'),
+    prevent_inital_callback=True
+)
+def update_histogram(year_range):
+    index_range = [(year - 2018)*12 for year in year_range]
+    return get_histogram(index_range)
+
+@app.callback(
+    Output('from-year','children'),
+    Output('to-year','children'),
+    Input('year-slider','value')
+)
+def update_year_text(year_range):
+    return str(year_range[0]), str(year_range[1])
+
+# @app.callback(
+#     Output('bar-graph','figure'),
+#     Input('year-slider','value'),
+#     prevent_inital_callback=True
+# )
+# def update_barplot(index_range):
+#     return get_barplot(index_range)
+
+if __name__ == '__main__':
+    app.run_server(debug=False, host='0.0.0.0', port=8080)
