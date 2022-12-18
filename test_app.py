0ba9413ff550bed5dc32d923c3ea55a73955c740
trymha <47079832+trymha@users.noreply.github.com>
2022-12-18 15:11:52 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -82,4 +82,4 @@ def update_year_text(year_range):
 #     return get_barplot(index_range)
 
 if __name__ == '__main__':
-    app.run_server(debug=False, host='0.0.0.0', port=8080)
+    app.run_server(debug=False)
