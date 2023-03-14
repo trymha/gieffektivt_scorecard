1896c456ad5ac257e6cf524f4a453e4b546f8b38
trymha <47079832+trymha@users.noreply.github.com>
2023-03-14 20:08:19 +0100

    Add files via upload

--- histogram_plot.py
+++ histogram_plot.py
@@ -4,32 +4,60 @@ import plotly.graph_objects as go
 from datetime import datetime, timedelta
 pd.set_option("display.precision", 0)
 
+def fixLeapYear(dates,donations):
+    isNotLeapYearDate = ~(dates=='02-29')
+    return dates[isNotLeapYearDate],donations[isNotLeapYearDate]
+
+hvr_tmpl = '<b>Donasjonsmenge:</b> %{x} NOK<br><b>Prosent:</b> %{y:.2f}%<extra></extra>'
+
 df = pd.read_csv('donations-effekt.csv', delimiter=";", parse_dates=['timestamp_confirmed'])
 df = df.sort_values(by='timestamp_confirmed')
 df.index = pd.to_datetime(df['timestamp_confirmed'])
+#df['recurring'] = [dNum[np.where(unique_Donor_ID==donor_ID)[0][0]]>1 for donor_ID in full_df['Donor_ID']]['Num_dons_ID'] =
 
+# Filter out recurring donations
+ID_num_count = df['Donor_ID'].value_counts()
+recurring_IDs = [ind for ind,val in zip(ID_num_count.index, ID_num_count.values) if val>1]
+df_reccuring = df.loc[df['Donor_ID'].isin(recurring_IDs)]
+
+#Get monthly donations for all, and recurring donations
 MDs = pd.DataFrame() #monthly donations
 MDs['sum_confirmed'] = df['sum_confirmed'].resample('M').sum() #Resample by day
 MDs['date_name'] = [ts.month_name()[:3] + ' ' + str(ts.year) for ts in MDs.index]
 MDs['timestamp'] = MDs.index
 MDs.index = [i for i in range(0,len(MDs))]
-#df['timestamp_confirmed'] = pd.to_datetime(df['timestamp_confirmed'])
 
-cumulative_donations = np.cumsum( df['sum_confirmed'].to_numpy() )
-donations = df['sum_confirmed']
-timestamps = pd.to_datetime(df['timestamp_confirmed'])
+MDs_reccuring = pd.DataFrame() #monthly donations
+MDs_reccuring['sum_confirmed'] = df_reccuring['sum_confirmed'].resample('M').sum() #Resample by day
+MDs_reccuring['date_name'] = [ts.month_name()[:3] + ' ' + str(ts.year) for ts in MDs_reccuring.index]
+MDs_reccuring['timestamp'] = MDs_reccuring.index
+MDs_reccuring.index = [i for i in range(0,len(MDs_reccuring))]
 
-hvr_tmpl = '<b>Donasjonsmenge:</b> %{x} NOK<br><b>Prosent:</b> %{y:.2f}%<extra></extra>'
+# Get yearly donations and sort by year
+DDs = pd.DataFrame() #daily donations
+DDs['value'] = df['sum_confirmed'].resample('D').sum() #Resample by day
+DDs['y-m-d'] = pd.to_datetime(DDs.index)
+DDs['y']= DDs['y-m-d'].dt.strftime('%Y')
+DDs['m-d'] = DDs['y-m-d'].dt.strftime('%m') + "-" + DDs['y-m-d'].dt.strftime('%d')
 
-def get_df_subset_by_month(month_index_range):
-    all_dates = MDs.iloc[month_index_range[0]:month_index_range[1]]
+year_strings = [str(yr) for yr in range(2019,2023)]
+dates_by_year     = [ DDs.loc[DDs['y']==year_str,'m-d'].to_numpy() for year_str in year_strings]
+donations_by_year = [ DDs.loc[DDs['y']==year_str,'value'].to_numpy() for year_str in year_strings]
+dates_by_year,donations_by_year = zip(*[ fixLeapYear(dts,dons) for dts,dons in zip(dates_by_year,donations_by_year) ])
+
+yMax = max([sum(dons) for dons in donations_by_year])
+
+def get_df_subset_by_month(month_index_range, month_df, full_df):
+    all_dates = month_df.iloc[month_index_range[0]:month_index_range[1]]
     date_1 = all_dates['timestamp'].iloc[0]
     date_2 = all_dates['timestamp'].iloc[-1]
-    df_subset = df.loc[(df.index>date_1)&(df.index<date_2)]
+    df_subset = full_df.loc[(full_df.index>date_1)&(full_df.index<date_2)]
     return df_subset
 
-def get_histogram(month_index_range, n_bins=100):
-    df_subset = get_df_subset_by_month(month_index_range)
+def get_histogram(month_index_range, exclude_otd, n_bins=50):
+    month_df = MDs_reccuring if exclude_otd else MDs
+    full_df = df_reccuring if exclude_otd else df
+    df_subset = get_df_subset_by_month(month_index_range, month_df=month_df, full_df=full_df)
     donations = df_subset['sum_confirmed'].to_numpy()
     fig = go.Figure()
     fig.add_trace(
@@ -120,6 +148,44 @@ def get_barplot(month_index_range):
     #fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='LightPink')
     return fig
 
+yearly_dons_hovertemplate = '<b>Dato:</b> %{x}<br><b>Verdi:</b> %{y:,.0f} kr'
+def get_yearly_donations_plot():
+    fig = go.Figure()
+    for dates, dons, year_str in zip(dates_by_year, donations_by_year, year_strings):
+        dates = ['1970-'+d for d in dates]
+        color = 'black' if year_str=='2022' else 'grey'
+        dons_cumsum = np.cumsum(dons)
+        fig.add_trace(
+            go.Scatter(x=dates, y=dons_cumsum, marker_color=color, name=year_str, xhoverformat="%d %b", hovertemplate=yearly_dons_hovertemplate)
+        )
+        fig.add_trace(
+            go.Scatter(x=[dates[-1]], y=[dons_cumsum[-1]], name=year_str, marker_color=color, mode='markers', xhoverformat="%d %b", hovertemplate=yearly_dons_hovertemplate)
+        )
+        fig.add_annotation(
+            x=dates[-1], y=dons_cumsum[-1]+0.03*yMax, text=year_str, showarrow=False, font=dict(color=color)
+        )
+    fig.update_layout(
+        xaxis = dict(
+            tickformat = '%b',
+            dtick="M1", 
+            ticklabelmode="period", 
+            ticks='outside', 
+            #tickvals=[f'1970-{mon}-01' for mon in range(1,13)],
+            #tickvals = pd.date_range('1970-01', '1970-12', freq='MS'),
+            showgrid=False,
+            fixedrange=True, #Disabling zoom
+        ),
+        yaxis = dict(gridcolor='lightgrey', fixedrange=True), #Disabling zoom),
+        hoverlabel=dict(bgcolor="#fafafa"),
+        showlegend = False,
+        paper_bgcolor='rgba(0,0,0,0)',
+        plot_bgcolor='rgba(0,0,0,0)',
+        margin=dict(l=0, r=0, t=0, b=0),
+    )
+        #plt.plot(dates[-1], dons_cumsum[-1], color=color_, marker='.', markersize=10)
+        #txtBox = plt.text(dates[-1], dons_cumsum[-1] + 0.02*yMax, year_str, va='bottom',ha='center')
+    return fig
+    
 def histogram_timing():
     fig = get_histogram([0,5])
 
--- test_app.py
+++ test_app.py
@@ -2,8 +2,7 @@ from dash import Dash, html, dcc, Output, Input
 import dash_bootstrap_components as dbc
 #from organization_plots import *
 import histogram_plot as hp
-import cProfile
-import pstats
+
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
@@ -14,6 +13,7 @@ showlabel_months = ['January','April','July','October']
 time_options = ['Week','Year','Quarter','Month']
 time_dd = dcc.Dropdown(id='time-dd',value=time_options[0],options=[{'label':to,'value':to} for to in time_options])
 
+onetime_donations_switch = dbc.Switch(id='onetime-donations-switch',label='Eksluder enkeltdonasjoner', value=False)
 month_slider = dcc.RangeSlider(
         id='month-slider', 
         #marks=month_marks,
@@ -22,10 +22,12 @@ month_slider = dcc.RangeSlider(
         step=1,
         min=0,
         max=len(month_marks)-1,
-        allowCross= False, pushable=6, #Minimum 6 months range
+        allowCross= False,
+        pushable=6, #Minimum 6 months range
         #vertical=True, verticalHeight=700,
         #tooltip={"placement": "bottom", "always_visible": True},
-        className='slider'),
+        #style={'padding':'0px 0px 0px 0px'}
+        )
 
 year_slider = dcc.RangeSlider(
         id='year-slider', 
@@ -36,23 +38,32 @@ year_slider = dcc.RangeSlider(
         #tooltip={"placement": "bottom", "always_visible": True},
         )#className='rc-slider'),
 
-histogram_graph = dcc.Graph(id='histogram-graph', style={"width": "100%"})
+histogram_graph = dcc.Graph(
+    id='histogram-graph', 
+    style={"width": "100%"}, 
+    config={'displayModeBar': False} #Hide options for saving graph, zooming etc
+)
 bar_graph = dcc.Graph(id='bar-graph', style={"width": "100%"})
+yearly_donations_graph = dcc.Graph(
+    id='yearly-donations-graph', 
+    figure=hp.get_yearly_donations_plot(),
+    config={'displayModeBar': False}
+)
 
-app.layout = html.Div([
-    #time_dd,
-    #dcc.Slider(id='bins-slider', value=100, min=20, max=500, step=10, className='slider'),
-    html.Br(),
+#app.layout = html.Div([
+app.layout = dbc.Container([
     dbc.Row([
-        # dbc.Col(html.Span('2019', id='from-year'), width='auto'),
-        # dbc.Col(year_slider, width=9),
-        # dbc.Col(html.Span('2021', id='to-year'), width='auto'),
+        dbc.Col(onetime_donations_switch,  width='auto'),
         dbc.Col(html.Span(month_marks[0], id='from-month'), width='auto'),
-        dbc.Col(month_slider, width=9),
+        dbc.Col(month_slider, width=8, style={'padding':'0px 0px0px'}),
         dbc.Col(html.Span(month_marks[len(month_marks)-1], id='to-month'), width='auto'),
         #dbc.Col([dcc.RangeSlider(0, 20, marks=None, value=[5, 15])], style={'width':'30%'})
-    ]), #style = {"height": "100%", 'background-color':'yellow'}), 
+    ], align='center'), #style = {"height": "100%", 'background-color':'yellow'}), 
     dbc.Row(dbc.Col(histogram_graph)),
+    html.Br(),
+    html.Hr(),
+    html.Br(),
+    dbc.Row(dbc.Col(yearly_donations_graph)),
     ], style={'backgroundColor':'#fafafa'}
 )
 
@@ -60,11 +71,12 @@ app.layout = html.Div([
     Output('histogram-graph','figure'),
     #Input('year-slider','value'),
     Input('month-slider','value'),
+    Input('onetime-donations-switch','value'),
     prevent_inital_callback=True
 )
-def update_histogram(month_index_range):
+def update_histogram(month_index_range, exclude_otd):
     #index_range = [(year - 2018)*12 for year in year_range]
-    return hp.get_histogram(month_index_range)
+    return hp.get_histogram(month_index_range, exclude_otd=exclude_otd)
 
 # @app.callback(
 #     Output('from-year','children'),
@@ -73,14 +85,6 @@ def update_histogram(month_index_range):
 # )
 # def update_year_text(year_range):
 #     return str(year_range[0]), str(year_range[1])
-
-@app.callback(
-    Output('from-month','children'),
-    Output('to-month','children'),
-    Input('month-slider','value')
-)
-def update_month_text(month_range):
-    return month_marks[month_range[0]], month_marks[month_range[1]]
 
 # @app.callback(
 #     Output('bar-graph','figure'),
